987a06101377d3ec7fb7b5c12dd60ad9e4881643
Daniel Milton <danielmilton@daniels-macbook-pro.local>
2017-01-21 21:27:05 -0800

    Pop up message initial code

--- Class.e beta/ViewController.swift
+++ Class.e beta/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var addClassView: UIView!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        addClassView.layer.cornerRadius = 5
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func animateIn() {
+        self.view.addSubview(addClassView)
+        addClassView.center = self.view.center
+        
+        addClassView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addClassView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4){
+            
+            
+            self.addClassView.alpha = 1
+            self.addClassView.transform = CGAffineTransform.identity
+        }
+        
+    }
+    
+    func animateOut (){
+        UIView.animate(withDuration: 0.3, animations: {
+            self.addClassView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.addClassView.alpha = 0
+            
+        }) { (successs:Bool) in
+            self.addClassView.removeFromSuperview()
+    }
+    }
 
+    @IBAction func dismissPopUp(_ sender: Any) {
+        animateOut()
+    }
+    @IBAction func addClass(_ sender: Any) {
+        animateIn()
+    }
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
